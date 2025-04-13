1a60c1e6ceedef1bfec58bb12b12b7bab85d6d4f
el1dor <is927861@gmail.com>
2025-04-13 19:30:29 +0300

    INIT

--- app/src/main/java/com/example/library_with_fragment/ItemViewModelFactory.kt
+++ app/src/main/java/com/example/library_with_fragment/ItemViewModelFactory.kt
@@ -1,14 +1,14 @@
-package com.example.library_with_fragment
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-
-class ItemViewModelFactory() : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ItemViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return ItemViewModel() as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
+//package com.example.library_with_fragment
+//
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.ViewModelProvider
+//
+//class ItemViewModelFactory() : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(ItemViewModel::class.java)) {
+//            @Suppress("UNCHECKED_CAST")
+//            return ItemViewModel() as T
+//        }
+//        throw IllegalArgumentException("Unknown ViewModel class")
+//    }
+//}
