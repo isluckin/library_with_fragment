754a2015d54ae7f5f50b25d3e31cce3d348fdc78
el1dor <is927861@gmail.com>
2025-04-20 10:29:56 +0300

    --

--- app/src/main/java/com/example/library_with_fragment/ItemViewModel.kt
+++ app/src/main/java/com/example/library_with_fragment/ItemViewModel.kt
@@ -1,134 +1,133 @@
-//package com.example.library_with_fragment
-//
-//import android.Manifest
-//import android.app.AlertDialog
-//import android.content.Context
-//import android.util.Log
-//import android.widget.Toast
-//import androidx.compose.material3.Snackbar
-//import androidx.compose.runtime.currentCompositionLocalContext
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//import androidx.lifecycle.viewModelScope
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.delay
-//import kotlinx.coroutines.flow.Flow
-//import kotlinx.coroutines.flow.MutableStateFlow
-//import kotlinx.coroutines.launch
-//import kotlinx.coroutines.withContext
-//import kotlin.coroutines.coroutineContext
-//import kotlin.math.log
-//import kotlin.random.Random
-//import kotlin.random.nextInt
-//
-//class ItemViewModel : ViewModel() {
-//    private val _items = MutableLiveData<List<Item>>()
-//    val items: LiveData<List<Item>> = _items
-//
-//    private val _selectedItem = MutableLiveData<Item?>()
-//    val selectedItem: LiveData<Item?> = _selectedItem
-//
-//    private val _scrollPosition = MutableLiveData<Int>()
-//    val scrollPosition: LiveData<Int> = _scrollPosition
-//
-//    private val _scrollToLast = MutableLiveData(false)
-//    val scrollToLast: LiveData<Boolean> = _scrollToLast
-//
-//    var currentlyEditingItem: Item? = null
-//    var isInEditMode: Boolean = false
-//    private val _isLoading = MutableStateFlow<Boolean>(false)
-//    var isLoading: MutableStateFlow<Boolean> = _isLoading
-//
-//    private val _errorEvent = MutableLiveData<String?>()
-//    val errorEvent: LiveData<String?> = _errorEvent
-//
-//    private var errorPeriod  = getRandom()
-//    private var operationCount: Int = 0
-//
-//    suspend fun selectItem(item: Item) {
-//        try {
-//            operationCount++
-//            Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
-//            if (operationCount >= errorPeriod) {
-//                Log.d("!!!", "FAKE EXEPTION")
-//                errorPeriod = getRandom()
-//                throw IllegalStateException("Random error")
-//            }
-//            delay(Random.nextLong(from = 100, until = 2001))
-//            _selectedItem.value = item
-//        }catch (e: Exception){
-//            operationCount=0
-//            _errorEvent.postValue("Failed to add item: ${e.message}")
-//        }
-//    }
-//    suspend fun addItem(item: Item) = withContext(Dispatchers.IO){
-//        try {
-//            operationCount++
-//            Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
-//            if (operationCount >= errorPeriod ) {
-//                Log.d("!!!", "FAKE EXEPTION")
-//                errorPeriod  = getRandom()
-//                throw IllegalStateException("Random error")
-//            }
-//            val currentItems = _items.value?.toMutableList() ?: mutableListOf()
-//            currentItems.add(item)
-//            _items.value = currentItems
-//            _scrollPosition.value = currentItems.size - 1
-//            _scrollToLast.value = true
-//        } catch (e: Exception) {
-//            operationCount=0
-//            _errorEvent.postValue("Failed to add item: ${e.message}")
-//        }
-//    }
-//
-//    fun clearError() {
-//        _errorEvent.value = null
-//    }
-//
-//
-//    fun setScrollPos(position: Int) {
-//        _scrollPosition.value = position
-//    }
-//
-//    fun resetScrollFlag() {
-//        _scrollToLast.value = false
-//    }
-//
-//    suspend fun loadItems() = withContext(Dispatchers.IO){
-//        viewModelScope.launch {
-//            try {
-//                operationCount++
-//                Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
-//                if (operationCount >= errorPeriod) {
-//                    Log.d("!!!", "FAKE EXEPTION")
-//                    errorPeriod  = getRandom()
-//                    throw IllegalStateException("Random error")
-//                }
-//                _isLoading.value = true
-//                delay(Random.nextLong(from = 100, until = 2001))
-//                _items.value = generateItems()
-//                _isLoading.value = false
-//            }
-//            catch (e: Exception){
-//                operationCount=0
-//                _errorEvent.postValue("Failed to add item: ${e.message}")
-//            }
-//        }
-//    }
-//    private fun getRandom(): Int  =  Random.nextInt(2, 6)
-//
-//    fun generateItems(): List<Item> {
-//        return listOf(
-//            Book(1, "mybook1", "I", 20, true, R.drawable.book_image),
-//            Disk(2, "diisk1", "DVD", false, R.drawable.disk_image),
-//            Disk(22, "diisk2", "СD", true, R.drawable.disk_image),
-//            Disk(23, "diisk3", "DVD", true, R.drawable.disk_image),
-//            Newspaper(31, "news1", 412, "April", true, R.drawable.newspaper_image),
-//            Newspaper(32, "news2", 412, "March", true, R.drawable.newspaper_image),
-//            Newspaper(33, "news3", 412, "June", true, R.drawable.newspaper_image),
-//            Book(12, "mybook2", "I", 20, false, R.drawable.book_image),
-//            Book(123, "mybook3", "I", 20, true, R.drawable.book_image)
-//        )
-//    }
-//}
+package com.example.library_with_fragment
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.material3.Snackbar
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.coroutineContext
+import kotlin.math.log
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class ItemViewModel : ViewModel() {
+    private val _items = MutableLiveData<List<Item>>()
+    val items: LiveData<List<Item>> = _items
+
+    private val _selectedItem = MutableLiveData<Item?>()
+    val selectedItem: LiveData<Item?> = _selectedItem
+
+    private val _scrollPosition = MutableLiveData<Int>()
+    val scrollPosition: LiveData<Int> = _scrollPosition
+
+    private val _scrollToLast = MutableLiveData(false)
+    val scrollToLast: LiveData<Boolean> = _scrollToLast
+
+    var currentlyEditingItem: Item? = null
+    var isInEditMode: Boolean = false
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    var isLoading: MutableStateFlow<Boolean> = _isLoading
+
+    private val _errorEvent = MutableLiveData<String?>()
+    val errorEvent: LiveData<String?> = _errorEvent
+
+    private var errorPeriod  = getRandom()
+    private var operationCount: Int = 0
+
+     fun selectItem(item: Item) {
+        try {
+            operationCount++
+            Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
+            if (operationCount >= errorPeriod) {
+                Log.d("!!!", "FAKE EXEPTION")
+                errorPeriod = getRandom()
+                throw IllegalStateException("Random error")
+            }
+            _selectedItem.value = item
+        }catch (e: Exception){
+            operationCount=0
+            _errorEvent.postValue("Failed to add item: ${e.message}")
+        }
+    }
+    suspend fun addItem(item: Item) = withContext(Dispatchers.IO){
+        try {
+            operationCount++
+            Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
+            if (operationCount >= errorPeriod ) {
+                Log.d("!!!", "FAKE EXEPTION")
+                errorPeriod  = getRandom()
+                throw IllegalStateException("Random error")
+            }
+            val currentItems = _items.value?.toMutableList() ?: mutableListOf()
+            currentItems.add(item)
+            _items.value = currentItems
+            _scrollPosition.value = currentItems.size - 1
+            _scrollToLast.value = true
+        } catch (e: Exception) {
+            operationCount=0
+            _errorEvent.postValue("Failed to add item: ${e.message}")
+        }
+    }
+
+    fun clearError() {
+        _errorEvent.value = null
+    }
+
+
+    fun setScrollPos(position: Int) {
+        _scrollPosition.value = position
+    }
+
+    fun resetScrollFlag() {
+        _scrollToLast.value = false
+    }
+
+    suspend fun loadItems() = withContext(Dispatchers.IO){
+        viewModelScope.launch {
+            try {
+                operationCount++
+                Log.d("!!!", "Operation: $operationCount,  Period: ${errorPeriod}")
+                if (operationCount >= errorPeriod) {
+                    Log.d("!!!", "FAKE EXEPTION")
+                    errorPeriod  = getRandom()
+                    throw IllegalStateException("Random error")
+                }
+                _isLoading.value = true
+                delay(Random.nextLong(from = 100, until = 2001))
+                _items.value = generateItems()
+                _isLoading.value = false
+            }
+            catch (e: Exception){
+                operationCount=0
+                _errorEvent.postValue("Failed to add item: ${e.message}")
+            }
+        }
+    }
+    private fun getRandom(): Int  =  Random.nextInt(2, 6)
+
+    fun generateItems(): List<Item> {
+        return listOf(
+            Book(1, "mybook1", "I", 20, true, R.drawable.book_image),
+            Disk(2, "diisk1", "DVD", false, R.drawable.disk_image),
+            Disk(22, "diisk2", "СD", true, R.drawable.disk_image),
+            Disk(23, "diisk3", "DVD", true, R.drawable.disk_image),
+            Newspaper(31, "news1", 412, "April", true, R.drawable.newspaper_image),
+            Newspaper(32, "news2", 412, "March", true, R.drawable.newspaper_image),
+            Newspaper(33, "news3", 412, "June", true, R.drawable.newspaper_image),
+            Book(12, "mybook2", "I", 20, false, R.drawable.book_image),
+            Book(123, "mybook3", "I", 20, true, R.drawable.book_image)
+        )
+    }
+}
--- app/src/main/java/com/example/library_with_fragment/MainActivity.kt
+++ app/src/main/java/com/example/library_with_fragment/MainActivity.kt
@@ -1,150 +1,150 @@
-//package com.example.library_with_fragment
-//
-//import android.app.AlertDialog
-//import android.content.res.Configuration
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.widget.RadioGroup
-//import androidx.activity.viewModels
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.lifecycle.lifecycleScope
-//import com.example.library_with_fragment.databinding.ActivityMainBinding
-//import kotlinx.coroutines.launch
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    private lateinit var binding: ActivityMainBinding
-//    private val viewModel: ItemViewModel by viewModels()
-//
-//    private val isLandscape: Boolean
-//        get() = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        lifecycleScope.launch {
-//            viewModel.loadItems()
-//            setup(savedInstanceState)
-//        }
-//
-//
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//
-//        binding.addButton.setOnClickListener {
-//            lifecycleScope.launch {
-//                showDialog()
-//            }
-//        }
-//
-//        viewModel.selectedItem.observe(this) { item ->
-//            if (item != null) {
-//                val detailFragment = DetailFragment.newInstance(item)
-//                if (isLandscape) {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.detail_container, detailFragment).commit()
-//                } else {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.single_container, detailFragment).addToBackStack(null)
-//                        .commit()
-//                }
-//            }
-//        }
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        val listFragment = supportFragmentManager.findFragmentById(R.id.list_container)
-//        val detailFragment = supportFragmentManager.findFragmentById(R.id.detail_container)
-//
-//        if (listFragment != null) {
-//            supportFragmentManager.putFragment(outState, "listFragment", listFragment)
-//        }
-//        if (detailFragment != null) {
-//            supportFragmentManager.putFragment(outState, "detailFragment", detailFragment)
-//        }
-//    }
-//
-//    private fun showDialog() {
-//        val dialogView = LayoutInflater.from(this).inflate(R.layout.menu, null)
-//        val radioGroup = dialogView.findViewById<RadioGroup>(R.id.radioGroup)
-//
-//        val dialog = AlertDialog.Builder(this).setTitle("Add new Item").setView(dialogView)
-//            .setPositiveButton("Add") { _, _ ->
-//                val selectedItem = when (radioGroup.checkedRadioButtonId) {
-//                    R.id.addBook -> Book.createEmptyBook()
-//                    R.id.addNews -> Newspaper.createEmptyNewspaper()
-//                    R.id.addDisk -> Disk.createEmptyDisk()
-//                    else -> null
-//                }
-//
-//                selectedItem?.let {
-//                    viewModel.currentlyEditingItem = it
-//                    viewModel.isInEditMode = true
-//
-//                    val detailFragment = DetailFragment.newInstance(it, isEditMode = true)
-//
-//                    val containerId =
-//                        if (isLandscape) R.id.detail_container else R.id.single_container
-//
-//                    supportFragmentManager.beginTransaction().replace(containerId, detailFragment)
-//                        .addToBackStack(null).commit()
-//                }
-//            }.setNegativeButton("Cancel") { dialog, _ ->
-//                dialog.dismiss()
-//            }.create()
-//
-//        dialog.show()
-//    }
-//    private fun setup(state: Bundle?)
-//    {
-//        if (viewModel.isInEditMode && viewModel.currentlyEditingItem != null) {
-//            val detailFragment = DetailFragment.newInstance(
-//                viewModel.currentlyEditingItem, isEditMode = true
-//            )
-//            if (isLandscape) {
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.list_container, ListFragment()).commit()
-//
-//            } else {
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.single_container, detailFragment).addToBackStack(null).commit()
-//            }
-//        }
-//        if (state == null) {
-//            if (isLandscape) {
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.list_container, ListFragment())
-//                    .replace(R.id.detail_container, DetailFragment.newInstance(null)).commit()
-//            } else {
-//                supportFragmentManager.beginTransaction()
-//                    .replace(R.id.single_container, ListFragment()).commit()
-//            }
-//        } else {
-//            if (isLandscape) {
-//                val listFragment = supportFragmentManager.findFragmentByTag("listFragment")
-//                val detailFragment = supportFragmentManager.findFragmentByTag("detailFragment")
-//
-//                if (listFragment == null) {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.list_container, ListFragment(), "listFragment").commit()
-//                }
-//
-//                if (detailFragment == null) {
-//                    supportFragmentManager.beginTransaction().replace(
-//                        R.id.detail_container, DetailFragment.newInstance(null), "detailFragment"
-//                    ).commit()
-//                }
-//            } else {
-//                val listFragment = supportFragmentManager.findFragmentByTag("listFragment")
-//
-//                if (listFragment == null) {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.single_container, ListFragment(), "listFragment").commit()
-//                }
-//            }
-//        }
-//
-//    }
-//}
+package com.example.library_with_fragment
+
+import android.app.AlertDialog
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.RadioGroup
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.library_with_fragment.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: ItemViewModel by viewModels()
+
+    private val isLandscape: Boolean
+        get() = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.loadItems()
+            setup(savedInstanceState)
+        }
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.addButton.setOnClickListener {
+            lifecycleScope.launch {
+                showDialog()
+            }
+        }
+
+        viewModel.selectedItem.observe(this) { item ->
+            if (item != null) {
+                val detailFragment = DetailFragment.newInstance(item)
+                if (isLandscape) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.detail_container, detailFragment).commit()
+                } else {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.single_container, detailFragment).addToBackStack(null)
+                        .commit()
+                }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val listFragment = supportFragmentManager.findFragmentById(R.id.list_container)
+        val detailFragment = supportFragmentManager.findFragmentById(R.id.detail_container)
+
+        if (listFragment != null) {
+            supportFragmentManager.putFragment(outState, "listFragment", listFragment)
+        }
+        if (detailFragment != null) {
+            supportFragmentManager.putFragment(outState, "detailFragment", detailFragment)
+        }
+    }
+
+    private fun showDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.menu, null)
+        val radioGroup = dialogView.findViewById<RadioGroup>(R.id.radioGroup)
+
+        val dialog = AlertDialog.Builder(this).setTitle("Add new Item").setView(dialogView)
+            .setPositiveButton("Add") { _, _ ->
+                val selectedItem = when (radioGroup.checkedRadioButtonId) {
+                    R.id.addBook -> Book.createEmptyBook()
+                    R.id.addNews -> Newspaper.createEmptyNewspaper()
+                    R.id.addDisk -> Disk.createEmptyDisk()
+                    else -> null
+                }
+
+                selectedItem?.let {
+                    viewModel.currentlyEditingItem = it
+                    viewModel.isInEditMode = true
+
+                    val detailFragment = DetailFragment.newInstance(it, isEditMode = true)
+
+                    val containerId =
+                        if (isLandscape) R.id.detail_container else R.id.single_container
+
+                    supportFragmentManager.beginTransaction().replace(containerId, detailFragment)
+                        .addToBackStack(null).commit()
+                }
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.create()
+
+        dialog.show()
+    }
+    private fun setup(state: Bundle?)
+    {
+        if (viewModel.isInEditMode && viewModel.currentlyEditingItem != null) {
+            val detailFragment = DetailFragment.newInstance(
+                viewModel.currentlyEditingItem, isEditMode = true
+            )
+            if (isLandscape) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.list_container, ListFragment()).commit()
+
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.single_container, detailFragment).addToBackStack(null).commit()
+            }
+        }
+        if (state == null) {
+            if (isLandscape) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.list_container, ListFragment())
+                    .replace(R.id.detail_container, DetailFragment.newInstance(null)).commit()
+            } else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.single_container, ListFragment()).commit()
+            }
+        } else {
+            if (isLandscape) {
+                val listFragment = supportFragmentManager.findFragmentByTag("listFragment")
+                val detailFragment = supportFragmentManager.findFragmentByTag("detailFragment")
+
+                if (listFragment == null) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.list_container, ListFragment(), "listFragment").commit()
+                }
+
+                if (detailFragment == null) {
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.detail_container, DetailFragment.newInstance(null), "detailFragment"
+                    ).commit()
+                }
+            } else {
+                val listFragment = supportFragmentManager.findFragmentByTag("listFragment")
+
+                if (listFragment == null) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.single_container, ListFragment(), "listFragment").commit()
+                }
+            }
+        }
+
+    }
+}
